e743e864eae84136e62bf0fb1a6bb47c22c85784
WuDaoTingFeng.yzh <2683849644@qq.com>
2023-08-29 14:17:55 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: WuDaoTingFeng.yzh 2683849644@qq.com
+ * @Date: 2023-08-29 14:09:54
+ * @LastEditors: WuDaoTingFeng.yzh 2683849644@qq.com
+ * @LastEditTime: 2023-08-29 14:16:07
+ * @FilePath: \olreact_yzh\src\App.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">React-App</div>
 }
 
 export default App;
